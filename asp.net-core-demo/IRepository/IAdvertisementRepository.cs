b4385cc438e3ac1bc22eb25620be74032b6ef5c5
Zhang Chen <329188614@qq.com>
2021-03-18 23:17:37 +0800

    新增demo

--- asp.net-core-demo/IRepository/IAdvertisementRepository.cs
+++ asp.net-core-demo/IRepository/IAdvertisementRepository.cs
@@ -5,12 +5,12 @@ using System.Linq.Expressions;
 
 namespace IRepository
 {
-    public interface IAdvertisementRepository
+    public interface IAdvertisementRepository:IBaseRepository<Advertisement>
     {
-        int Sum(int i, int j);
-        int Add(Advertisement model);
-        bool Delete(Advertisement model);
-        bool Update(Advertisement model);
-        List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression);
+        //int Sum(int i, int j);
+        //int Add(Advertisement model);
+        //bool Delete(Advertisement model);
+        //bool Update(Advertisement model);
+        //List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression);
     }
 }
