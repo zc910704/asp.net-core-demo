9b77acbdd512ecc233ba96033a332b41bcbed136
Zhang Chen <329188614@qq.com>
2021-03-20 23:41:38 +0800

    新增demo

--- asp.net-core-demo/Repository/AdvertisementRepository.cs
+++ asp.net-core-demo/Repository/AdvertisementRepository.cs
@@ -5,6 +5,7 @@ using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Repository
 {
@@ -63,6 +64,111 @@ namespace Repository
             //这种方式会以主键为条件
             var i = db.Updateable(model).ExecuteCommand();
             return i > 0;
+        }
+
+        public Task<Advertisement> QueryByID(object objId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Advertisement> QueryByID(object objId, bool blnUseCache = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> QueryByIDs(object[] lstIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IBaseRepository<Advertisement>.Add(Advertisement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IBaseRepository<Advertisement>.Delete(Advertisement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteByIds(object[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IBaseRepository<Advertisement>.Update(Advertisement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Advertisement entity, string strWhere)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Advertisement entity, List<string> lstColumns = null, List<string> lstIgnoreColumns = null, string strWhere = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(string strWhere)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Advertisement>> IBaseRepository<Advertisement>.Query(Expression<Func<Advertisement, bool>> whereExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(Expression<Func<Advertisement, bool>> whereExpression, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(Expression<Func<Advertisement, bool>> whereExpression, Expression<Func<Advertisement, object>> orderByExpression, bool isAsc = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(string strWhere, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(Expression<Func<Advertisement, bool>> whereExpression, int intTop, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(string strWhere, int intTop, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(Expression<Func<Advertisement, bool>> whereExpression, int intPageIndex, int intPageSize, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> Query(string strWhere, int intPageIndex, int intPageSize, string strOrderByFileds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Advertisement>> QueryPage(Expression<Func<Advertisement, bool>> whereExpression, int intPageIndex = 0, int intPageSize = 20, string strOrderByFileds = null)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ asp.net-core-demo/SingletonScopeTransientDemo/Service/Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingletonScopeTransientDemo
+{
+    public interface ISingTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+
+    public class SingTest : ISingTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    //--------------------------
+
+    public interface ISconTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+    public class SconTest : ISconTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    //--------------------------
+    public interface ITranTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+    public class TranTest : ITranTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    //-----------------------
+    public interface IAService
+    {
+        void RedisTest();
+    }
+
+    public class AService : IAService
+    {
+        private ISingTest sing; ITranTest tran; ISconTest scon;
+        public AService(ISingTest sing, ITranTest tran, ISconTest scon)
+        {
+            this.sing = sing;
+            this.tran = tran;
+            this.scon = scon;
+        }
+        public void RedisTest()
+        {
+
+        }
+    }
+}
